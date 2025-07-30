9e116bfe1efb286cc62fb04f53eedbd44a7b9a75
joaoflorencoJS <joaoflorenco321@gmail.com>
2025-07-30 16:32:06 -0300

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Parabéns por ter chegado até aqui, amigo! Faça uma pausa, sente-se e
+      descanse por um momento antes de retornar as atividades!
+    </h1>
+  );
+}
+
+export default Home;
