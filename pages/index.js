df045af9ab1f7235b9efcf14ae6e0c06171ecd8a
joaoflorencoJS <joaoflorenco321@gmail.com>
2025-08-01 16:26:28 -0300

    adiciona gif descanso

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 function Home() {
   return (
-    <h1>
-      Parabéns por ter chegado até aqui, amigo! Faça uma pausa, sente-se e
-      descanse por um momento antes de retornar as atividades!
-    </h1>
+    <>
+      <h1>
+        Parabéns por ter chegado até aqui, amigo! Faça uma pausa, sente-se e
+        descanse por um momento antes de retornar as atividades!
+      </h1>
+
+      <img src="https://i.redd.it/xte6gka2ghj91.gif" alt="" width={500} />
+    </>
   );
 }
 
